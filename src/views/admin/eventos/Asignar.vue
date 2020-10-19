851604f2b684af7e4d49942e427c73417887f944
eduvzb <60900038+eduvzb@users.noreply.github.com>
2020-10-19 17:17:44 -0500

    seleccionar evento

--- src/views/admin/eventos/Asignar.vue
+++ src/views/admin/eventos/Asignar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="mx-auto" max-width="900">
+  <v-card class="mx-auto" max-width="600">
     <v-card-title class="ma-0 title font-weight-regular justify-space-between">
       <span>{{ currentTitle }}</span>
       <v-avatar
@@ -12,9 +12,19 @@
     <v-window v-model="step">
       <v-window-item :value="1">
         <v-container>
-          <v-row>
+          <v-row align-content="justify">
             <v-col cols="12" md="6">
-              <v-text-field type="date" value="as"></v-text-field>
+              <v-text-field type="date" value="aasds"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row align-content="justify">
+            <v-col cols="12" md="12">
+              <v-data-table
+                :headers="headers"
+                :items="desserts"
+                :items-per-page="5"
+                class="elevation-1"
+              ></v-data-table>
             </v-col>
           </v-row>
         </v-container>
@@ -45,23 +55,108 @@
 </template>
 
 <script>
-export default {
-  name: "Asignar",
-  data: () => ({
-    step: 1
-  }),
-
-  computed: {
-    currentTitle() {
-      switch (this.step) {
-        case 1:
-          return "Buscar evento";
-        case 2:
-          return "Create a password";
-        default:
-          return "Account created";
+  export default {
+    data () {
+      return {
+        step: 1,
+        headers: [
+          { text: 'Id', value: 'carbs' },
+          { text: 'Nombre', value: 'protein' },
+          { text: 'Fecha', value: 'iron' },
+          { text: 'Tipo', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
       }
-    }
+    },
+    computed: {
+      currentTitle () {
+        switch (this.step) {
+          case 1: return 'Sign-up'
+          case 2: return 'Create a password'
+          default: return 'Account created'
+        }
+      },
+    },
   }
-};
 </script>
