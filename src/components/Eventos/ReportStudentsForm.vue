aaf35a958946ec2dd240752bbcf89d702d8018c3
Manuel de Jesus Alavazarez Hernández <eemanuel_11@hotmail.com>
2020-11-19 20:49:26 -0600

    Vista-Componente-Reportes-Eventos
    Se desarrollo la vista para reportes de eventos y los componentes

---
+++ src/components/Eventos/ReportStudentsForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn depressed color="primary" rounded v-bind="attrs" v-on="on">
+        <v-icon dark>mdi-arrow-right-bold-circle</v-icon>
+        </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Estudiantes que asistieron a un evento extraescolar
+        </span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row align="center" justify="space-around">
+            <v-col class="d-flex" cols="12" sm="8">
+              <v-select
+                :items="items"
+                label="Sexo"
+                solo
+              ></v-select>
+            </v-col>
+          </v-row>
+          <v-row align="center" justify="space-around">
+            <v-col class="d-flex" cols="12" sm="8">
+              <v-select
+                :items="items"
+                label="Carrera"
+                solo
+              ></v-select>
+            </v-col>
+          </v-row>
+          <v-row align="center"
+    justify="space-around">
+              <div class="my-2">
+            <v-btn
+              color="success"
+              fab
+              x-large
+              dark
+            >
+              <v-icon>mdi-domain</v-icon>
+            </v-btn>
+          </div>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm",
+  data: () => ({dialog:false})
+};
+</script>
--- src/views/admin/eventos/Reports.vue
+++ src/views/admin/eventos/Reports.vue
@@ -1,9 +1,54 @@
 <template>
-  <v-card>
+  <v-card height="280">
     <v-card-title class="heading-2">
       Reportes Estadisticos
       <v-spacer></v-spacer>
     </v-card-title>
+    <v-card class="mx-auto" max-height="155" max-width="530" qoutlined>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-medium">
+            1. Estudiantes que asistieron a un evento extraescolar
+        </v-list-item-title>
+      </v-list-item-content>
+      <v-card-actions>
+        <ReportStudentsForm />
+      </v-card-actions>
+      </v-list-item>
+   
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-medium">
+            2. Eventos realizados en un dia especifico
+        </v-list-item-title>
+      </v-list-item-content>
+      <v-card-actions>
+        <v-btn depressed color="primary" rounded>
+        <v-icon dark>mdi-arrow-right-bold-circle</v-icon>
+        </v-btn>
+      </v-card-actions>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-medium">
+            3. Eventos realizados en un periodo de fechas
+        </v-list-item-title>
+      </v-list-item-content>
+      <v-card-actions>
+        <v-btn depressed color="primary" rounded>
+        <v-icon dark>mdi-arrow-right-bold-circle</v-icon>
+        </v-btn>
+      </v-card-actions>
+      </v-list-item>
+    </v-card>
   </v-card>
 </template>
+
+<script>
+import ReportStudentsForm from "@/components/Eventos/ReportStudentsForm";
+export default {
+  components: { ReportStudentsForm },
+}
+</script>
 
