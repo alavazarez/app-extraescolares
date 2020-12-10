8fa7e5bcff3b346b9ef564a10af9a19e97e82571
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2020-12-09 23:06:33 -0600

    asignar eventos

--- src/views/admin/eventos/Asignar.vue
+++ src/views/admin/eventos/Asignar.vue
@@ -1,165 +1,158 @@
 <template>
-  <v-card class="mx-auto" max-width="1000">
-    <v-card-title class="ma-0 title font-weight-regular justify-space-between">
-      <span>{{ currentTitle }}</span>
-      <v-avatar
-        color="primary lighten-2"
-        class="subheading white--text"
-        size="24"
-        v-text="step"
-      ></v-avatar>
-    </v-card-title>
-    <v-window v-model="step">
-      <v-window-item :value="1">
-        <v-container>
-          <v-row align-content="justify">
-            <v-col cols="12" md="6">
-              <v-text-field type="date" value="aasds"></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row align-content="justify">
-            <v-col cols="12" md="12">
-              <v-data-table
-                :headers="headers"
-                :items="desserts"
-                :items-per-page="5"
-                class="elevation-1"
-              ></v-data-table>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-window-item>
-      <v-window-item :value="2">
-        <v-card-text>
-          <v-text-field label="Password" type="password"></v-text-field>
-          <v-text-field label="Confirm Password" type="password"></v-text-field>
-          <span class="caption grey--text text--darken-1">
-            Please enter a password for your account
-          </span>
-        </v-card-text>
-      </v-window-item>
-    </v-window>
+  <v-stepper v-model="e1"
+   non-linear 
+   >
+    <v-stepper-header>
+      <v-stepper-step
+         :complete="e1 > 1"
+        step="1"
+      >
+        Seleccionar evento
+      </v-stepper-step>
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <v-card-actions>
-      <v-btn :disabled="step === 1" text @click="step--">
-        Back
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn :disabled="step === 3" color="primary" depressed @click="step++">
-        Next
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-stepper-step
+        :complete="e1 > 1"
+        step="2"
+      >
+        Seleccionar alumnos
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step 
+      step="3"
+      :complete="e1 > 1"
+      editable
+      >
+        Confirmar asistencia
+      </v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-card
+          class="mb-2"
+        >
+          <v-card-title>
+            <v-row>
+           <v-col md="4">
+             <v-text-field
+             v-model="date" 
+             type="date"
+             regular label="Fecha del evento"></v-text-field>
+           </v-col>
+           <v-col md="4">
+             <v-select
+              :items="items"
+              label="Tipo de evento"
+              item-text="name"
+              item-value="id"
+              ></v-select>
+           </v-col>
+         </v-row>
+        </v-card-title>
+        <v-data-table
+        :headers="headers"
+        show-select
+        single-select
+        :items="events"
+        item-key="id"
+        :search="date"
+        ></v-data-table>
+        </v-card>
+        <v-btn
+          color="primary"
+          @click="e1 = 2"
+        >
+          Continuar
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-card
+          class="mb-12"
+          color="grey lighten-1"
+          height="200px"
+        ></v-card>
+
+        <v-btn
+          color="primary"
+          @click="e1 = 3"
+        >
+          Continuar
+        </v-btn>
+
+        <v-btn text
+        @click="e1 = e1-1"
+        >
+          Regresar
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card
+          class="mb-12"
+          color="grey lighten-1"
+          height="200px"
+        ></v-card>
+        <v-btn
+          color="primary"
+          @click="e1 = 1"
+        >
+          Guardar
+        </v-btn>
+
+        <v-btn text
+        @click="e1 = e1-1"
+        >
+          Regresar
+        </v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      step: 1,
-      headers: [
-        { text: "Id", value: "carbs" },
-        { text: "Nombre", value: "protein" },
-        { text: "Fecha", value: "iron" },
-        { text: "Tipo", value: "iron" }
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
-        }
-      ]
-    };
-  },
-  computed: {
-    currentTitle() {
-      switch (this.step) {
-        case 1:
-          return "Sign-up";
-        case 2:
-          return "Create a password";
-        default:
-          return "Account created";
+import { mapActions, mapGetters } from 'vuex'
+  export default {
+    data () {
+      return {
+        e1: 1,
+        date:null,
+        items: [
+          {id:1, name:'Deportivo'},
+          {id:2, name:'Cultural'},
+          {id:3, name:'Cívico'},
+        ],
+        headers: [
+          { text: 'Nombre', value: 'name' },
+          { text: 'Tipo de evento', value: 'type_event_id' },
+          { text: 'Organizador', value: 'organizer' },
+          { text: 'Lugar', value: 'place' },
+          { text: 'Fecha', value: 'date' },
+        ],
       }
-    }
+    },
+    mounted () {
+      this.getEvents();
+    },
+    computed:{
+       ...mapGetters({
+      events: 'event/events',
+    })
+    },
+    methods:{
+      ...mapActions('event',['store','getEvents']),
+    },
   }
-};
 </script>
+
+<style>
+
+</style>
